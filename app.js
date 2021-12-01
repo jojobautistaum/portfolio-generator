a766fc09aa1c6b84dcdada519aa20a20b3a4fd93
Jojo Bautista <gjojob@yahoo.com>
2021-12-01 09:16:12 -0600

    added generate projects

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-// const pageHTML = generatePage(name, github);
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 const inquirer = require('inquirer');
 
 const promptUser = () => {
@@ -133,6 +127,9 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
-  
