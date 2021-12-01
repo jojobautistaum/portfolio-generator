f18c7b73743de5017cfcf91c53012c580c164e89
Jojo Bautista <gjojob@yahoo.com>
2021-11-30 23:01:17 -0600

    added functionality to enter more projects

--- app.js
+++ app.js
@@ -31,7 +31,10 @@ const promptUser = () => {
   ]);
 };
 
-const promptProject = () => {
+const promptProject = portfolioData => {
+  if(!portfolioData.projects){
+    portfolioData.projects = [];
+  }
   console.log(`
   =================
   Add a New Project
@@ -68,13 +71,24 @@ const promptProject = () => {
     {
       type: 'confirm',
       name: 'confirmAddProject',
-      message: 'Would you like to enter antoher project? ',
+      message: 'Would you like to enter another project? ',
       default: false
     }
-  ]);
+  ])
+  .then(projectData => {
+    portfolioData.projects.push(projectData);
+    if (projectData.confirmAddProject) {
+      return promptProject(portfolioData);
+    } else {
+      return portfolioData;
+    }
+  });
+
 };
 
 promptUser()
-  .then(answers => console.log(answers))
   .then(promptProject)
-  .then(projectAnswers => console.log(projectAnswers));
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
+  
