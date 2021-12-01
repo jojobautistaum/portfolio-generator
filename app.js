e2ed7c8fe89d390f8318081c0b15647c3fb1bfb6
Jojo Bautista <gjojob@yahoo.com>
2021-11-30 23:12:10 -0600

    added validation to required questions

--- app.js
+++ app.js
@@ -16,7 +16,15 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name? '
+      message: 'What is your name? (Required) ',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name! ");
+          return false;
+        }
+      }
     },
     {
       type: 'input',
@@ -44,12 +52,28 @@ const promptProject = portfolioData => {
     {
       type: 'input',
       name: 'name',
-      message: 'What is the name of your project? '
+      message: 'What is the name of your project? (Required): ',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your project name! ");
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'description',
-      message: 'Provide a description of the project (Required): '
+      message: 'Provide a description of the project (Required): ',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your project description! ");
+          return false;
+        }
+      }
     },
     {
       type: 'checkbox',
@@ -60,7 +84,15 @@ const promptProject = portfolioData => {
     {
       type: 'input',
       name: 'link',
-      message: 'Enter the GitHub link to your project. (Required): '
+      message: 'Enter the GitHub link to your project. (Required): ',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your GitHub link! ");
+          return false;
+        }
+      }
     },
     {
       type: 'confirm',
